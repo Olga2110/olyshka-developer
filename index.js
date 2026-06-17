0928aac92e80ce05b2a03e18bf52e648110188e7
OlgaFirs <odanilona21@gmail.com>
2026-06-17 13:05:45 +0400

    feat:show ten tasks

--- index.js
+++ index.js
@@ -6,10 +6,11 @@ const all_tasks = document.querySelector('.all-tasks')
 
 fetch('https://jsonplaceholder.typicode.com/todos')
     .then(response => response.json())
-    .then(json => json.forEach((task) => {
+    .then(json => json.slice(0,9).forEach((task) => {
 
         const container = document.createElement('div');
         container.classList.add('task-list');
+        
         container.innerHTML =
             `<span>${task.title}</span> 
         <span>${task.completed}</span>`
